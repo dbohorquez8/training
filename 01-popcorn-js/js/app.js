27c7b654b78fc32e6ea351f233eee85b671b5b04
dbohorquez8 <dbohorquez8@gmail.com>
2014-03-19 21:28:08 -0500

    Added favorites list

--- 01-popcorn-js/js/app.js
+++ 01-popcorn-js/js/app.js
@@ -2,12 +2,13 @@
 
 var Popcorn = {
   movies: []
-}
+};
 
 Popcorn.apiKey = '?api_key=475d960c7374b0d07a51bf9f1e9cfd03';
 Popcorn.apiPath = 'https://api.themoviedb.org/3/movie/';
 Popcorn.imgPath = 'https://image.tmdb.org/t/p/w';
 
+//Service to make requests
 Popcorn.get = function (url){
   var deferred = new $.Deferred();
 
@@ -19,6 +20,7 @@ Popcorn.get = function (url){
 };
 
 Popcorn.getMovies = function (categories) {
+  //Gets the movies in the specified categories
   $.each(categories, function (index, category) {
     var url = Popcorn.apiPath + category + Popcorn.apiKey;
     var promise = Popcorn.get(url);
@@ -33,16 +35,25 @@ Popcorn.getMovies = function (categories) {
         if (!existingMovie) {
           // Adds the new movie to the list
           movie.categories = [category];
+
+          //Checks if movie has been added to Local Favorites
+          if(_.contains(JSON.parse(localStorage['favorites']), movie.id)){
+            movie.categories.push('favorites');
+          }
+
+          //Adds additional data for the movie
           movie.small_poster_url = Popcorn.imgPath + '92' + movie.poster_path;
           movie.large_poster_url = Popcorn.imgPath + '300' + movie.poster_path;
           movie.year = movie.release_date.substring(0, 4);
           movie.cast = Popcorn.getCast(movie.id);
+
           Popcorn.movies.push(movie);        
         } else {
           // Adds new category to the existing movie
           existingMovie.categories.push(category);
         }
       });
+
       $(Popcorn).trigger('modelReady:' + category);
     });
   });
@@ -68,11 +79,12 @@ Popcorn.getCast = function (movieID) {
 };
 
 Popcorn.listMovies = function (category, container) {
-  var movies = _.filter(Popcorn.movies, function (movie) {
-    return _.where(movie.categories, category);
-  });
+  $(container).html('');
 
-  var imgUrl = 'https://image.tmdb.org/t/p/w92';
+  //Finds movies belonging to the specified category
+  var movies = _.filter(Popcorn.movies, function (movie) {
+    return _.contains(movie.categories, category);
+  });
 
   $.each(movies, function (index, movie) {
     var movieElement = $('<a/>')
@@ -90,7 +102,7 @@ Popcorn.listMovies = function (category, container) {
 };
 
 Popcorn.showMovie = function (movieID) {
-  var container = $('.js-movie-details');
+  var container = $('.js-movie-overlay');
   container.fadeIn();
   var movie = _.findWhere(this.movies, { 'id': movieID });
 
@@ -114,11 +126,74 @@ Popcorn.showMovie = function (movieID) {
     castMember.append(castMemberName);
     castContainer.append(castMember);
   });
+
+  $('.js-favorite-button, .js-unfavorite-button').data('movie', movie.id);
+  $('.js-favorite-button, .js-unfavorite-button').hide();
+
+  if(_.contains(movie.categories, 'favorites')){
+    $('.js-unfavorite-button').show();
+  }else{
+    $('.js-favorite-button').show();
+  }
+};
+
+Popcorn.hideMovie = function () {
+  $('.js-movie-overlay').fadeOut();
+};
+
+Popcorn.addFavorite = function (movieID) {
+  var movie = _.findWhere(Popcorn.movies, { 'id': movieID });
+
+  movie.categories.push('favorites');
+
+  Popcorn.listMovies('favorites', '.js-favorites');
+
+  $('.js-favorite-button').hide();
+  $('.js-unfavorite-button').show();
+
+  var favorites = JSON.parse(localStorage['favorites']);
+  favorites.push(movie.id);
+  localStorage['favorites'] = JSON.stringify(favorites);
+};
+
+Popcorn.removeFavorite = function (movieID) {
+  var movie = _.findWhere(Popcorn.movies, { 'id': movieID });
+
+  movie.categories.pop('favorites');
+
+  Popcorn.listMovies('favorites', '.js-favorites');
+
+  $('.js-unfavorite-button').hide();
+  $('.js-favorite-button').show();
+  
+  var favorites = JSON.parse(localStorage['favorites']);
+  favorites.pop(movie.id);
+  localStorage['favorites'] = JSON.stringify(favorites);
 };
 
 function init() {
+  if(!localStorage['favorites']) localStorage.setItem('favorites', '[]');
+
   Popcorn.categories = ['popular', 'top_rated'];
   Popcorn.getMovies(Popcorn.categories);
+
+  $('.js-favorite-button').on('click', function (e){
+    e.preventDefault();
+    Popcorn.addFavorite($(this).data('movie'));
+  });
+
+  $('.js-unfavorite-button').on('click', function (e){
+    e.preventDefault();
+    Popcorn.removeFavorite($(this).data('movie'));
+  });
+
+  $('.js-movie-overlay').on('click', function (){
+    Popcorn.hideMovie();
+  });
+
+  $('.js-movie-details').on('click', function (e){
+    e.stopPropagation();
+  });
 }
 
 (function($) {
@@ -127,8 +202,8 @@ function init() {
   $.each(Popcorn.categories, function (index, category){
     $(Popcorn).on('modelReady:' + category, function (){
       Popcorn.listMovies(category, '.js-' + category);
+      Popcorn.listMovies('favorites', '.js-favorites');
     });
   });
   
 })(jQuery);
-
